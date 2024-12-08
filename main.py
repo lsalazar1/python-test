ba4c42c8b0769c24597f270d8b270fc089bcedc2
Rommel Salazar <salazro69@gmail.com>
2024-12-08 15:11:03 -0700

    Modifying main.py to add function to check if word is a palindrome

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+def isPalindrome(word):
+
+    modified_string="".join(char.lower() for char in word if char.isalnum())
+    if modified_string == modified_string[::-1]:
+        return True
+    return False
+
+
+word="Madam"
+print(isPalindrome(word))
